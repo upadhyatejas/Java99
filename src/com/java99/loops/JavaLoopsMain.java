1d6ced38f042b67721c62513448c2e84d6dc37f6
Tejas Upadhya <33410261+upadhyatejas@users.noreply.github.com>
2020-01-28 22:39:42 +0530

    [updated main]

--- src/com/java99/loops/JavaLoopsMain.java
+++ src/com/java99/loops/JavaLoopsMain.java
@@ -1,5 +1,20 @@
+//Answers to challenges on https://tech.tonyballantyne.com/java/99-java-problems/2-loops/
+
 package com.java99.loops;
 
 public class JavaLoopsMain {
+	public static void main(String[] args) {
+		JavaLoopsFunc javaLoops = new JavaLoopsFunc();
+		javaLoops.oneToTen();
+		javaLoops.oddNumbers();
+		javaLoops.squareNumbers();
+		javaLoops.randomNumbers();
+		javaLoops.triangle();
+		javaLoops.tableSquare();
+		javaLoops.tableSquares();
+		System.out.println("class hashcode");
+		System.out.println(javaLoops.hashCode());
+	}
 
 }
+
